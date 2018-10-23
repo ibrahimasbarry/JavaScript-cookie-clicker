2a7ad1716c74661dfeb1cf5de8515da595babe5a
MDE86 <michel.deraemaeker@gmail.com>
2018-10-23 17:26:13 +0200

    redo until stage 11

---
+++ js/script-1try.js
@@ -0,0 +1,51 @@
+let buttonClick = document.getElementById("clickbutton");
+let showScore = document.getElementById("show");
+let multiClick = document.getElementById("multi");
+let multiValue = document.getElementById("multivalue"); // can be removed ?????? not used atm
+
+let score = 0;
+let multiScore = 1;
+let decreaseScore = 5; // pentalty for clicking multiplicator
+
+// NORMAL COOKIE COUNTER
+
+function addScore() {
+    score += multiScore; //// add multiplication to cookie score
+    showScore.innerHTML = score;
+}
+
+buttonClick.addEventListener("click", addScore);
+
+// MULTIPLICATOR + CLICKING IS NOT FREE + add value to multiplicator button
+
+function clickNotFree() {
+
+    function multiMultiplicator(decreaseScore) {
+        decreaseScore *= 2;
+        return decreaseScore;
+    }
+    multiMultiplicator();
+
+    if (score > decreaseScore) { 
+        // putting increaseMulti inside the if prevents the multi counter from increasing when you click on it before it disappears and IF loop makes sure you can only use the multiplicator when enough credits (not negative)
+        
+        function increaseMulti() { 
+            multiScore++;   // increment the multiplicator value
+            document.querySelector(".multiplaceholder").innerHTML = "Multiplicator x" + multiScore; /// put multiplicator value in the multiplicator button
+        }
+        increaseMulti();
+        
+        score -= decreaseScore*2; // substract penalty from cookie score (minus)
+        showScore.innerHTML = score; // the updated score linked to the span with id show on html page
+        }
+    }
+
+multiClick.addEventListener("click", clickNotFree);
+
+// increase cost of multiplicator =>    decreaseScore = decreaseScore*2 repeat   ... 
+
+// for (let i = 0; i < decreaseScore.length; i++) {
+//     decreaseScore = decreaseScore*2;
+// }
+
+
--- js/script.js
+++ js/script.js
@@ -1,51 +1,34 @@
 let buttonClick = document.getElementById("clickbutton");
-let showScore = document.getElementById("show");
-let multiClick = document.getElementById("multi");
-let multiValue = document.getElementById("multivalue"); // can be removed ?????? not used atm
+let cookieCount = document.getElementById("show");
+let multiClick = document.getElementById("multibutton");
+let multiValue = document.getElementById("multiplyvalue");
 
-let score = 0;
+let cookieScore = 0;
 let multiScore = 1;
-let decreaseScore = 5; // pentalty for clicking multiplicator
+let penaltyScore = 5;
 
-// NORMAL COOKIE COUNTER
 
-function addScore() {
-    score += multiScore; //// add multiplication to cookie score
-    showScore.innerHTML = score;
+function clickTheButton() {
+    cookieScore += multiScore;
+    cookieCount.innerHTML = cookieScore;
+}
+buttonClick.addEventListener("click", clickTheButton);
+
+function increaseMultiplier() {
+    multiScore++;
+    document.querySelector("#multiplaceholder").innerHTML = `Multiplicator x ${multiScore} <br />On the next click the multiplier will cost you x ${penaltyScore*2}`;
 }
 
-buttonClick.addEventListener("click", addScore);
-
-// MULTIPLICATOR + CLICKING IS NOT FREE + add value to multiplicator button
-
-function clickNotFree() {
-
-    function multiMultiplicator(decreaseScore) {
-        decreaseScore *= 2;
-        return decreaseScore;
+function penaltyMultiplier() {
+    if (cookieScore > penaltyScore) {
+        cookieScore -= penaltyScore;
+        cookieCount.innerHTML = cookieScore;
+        increaseMultiplier();
+        doublePenalty();
     }
-    multiMultiplicator();
+}
+multiClick.addEventListener("click", penaltyMultiplier);
 
-    if (score > decreaseScore) { 
-        // putting increaseMulti inside the if prevents the multi counter from increasing when you click on it before it disappears and IF loop makes sure you can only use the multiplicator when enough credits (not negative)
-        
-        function increaseMulti() { 
-            multiScore++;   // increment the multiplicator value
-            document.querySelector(".multiplaceholder").innerHTML = "Multiplicator x" + multiScore; /// put multiplicator value in the multiplicator button
-        }
-        increaseMulti();
-        
-        score -= decreaseScore*2; // substract penalty from cookie score (minus)
-        showScore.innerHTML = score; // the updated score linked to the span with id show on html page
-        }
-    }
-
-multiClick.addEventListener("click", clickNotFree);
-
-// increase cost of multiplicator =>    decreaseScore = decreaseScore*2 repeat   ... 
-
-// for (let i = 0; i < decreaseScore.length; i++) {
-//     decreaseScore = decreaseScore*2;
-// }
-
-
+function doublePenalty() {
+    penaltyScore *= 2;
+}
