b10760fb5e2a38e982c7b7484ab3a399060f124f
MDE86 <michel.deraemaeker@gmail.com>
2018-10-24 15:00:34 +0200

    upd stage 12 incl

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Cookie-clicker
+# Javascript-Cookie-clicker
+
+A basic cookie clicker game made with JS. I really struggeled on when to call certain functions and how to make sure the autoclicker button was removed after having it clicked once. I learned to make separata functions for each step in the exercise, instead of nesting functions the whole time like I did during my first try (check script-1try.js in the repo).
+
+## Code used
+
+Javascript (& limited HTML and CSS for lay-out purposes)
+
+## Timing
+
+Made during the seventh week of my web development training at BeCode (october 2018).
+
+## Status
+
+Finished
+
+## Credits
+
+Thanks to Faysal for helping me out on numerous occasions!
--- js/script.js
+++ js/script.js
@@ -1,22 +1,26 @@
 let buttonClick = document.getElementById("clickbutton");
 let cookieCount = document.getElementById("show");
 let multiClick = document.getElementById("multibutton");
-let multiValue = document.getElementById("multiplyvalue");
+let autoClickButt = document.getElementById("autoclickbutton");
 
 let cookieScore = 0;
 let multiScore = 1;
 let penaltyScore = 5;
-
+let autoClicker;
+let autoClickLvl = 20;
+let autoClickCost = 25;
+let ticket = 1; // will help limit the number of times the button is shown to one
 
 function clickTheButton() {
     cookieScore += multiScore;
     cookieCount.innerHTML = cookieScore;
+    showHideButton();
 }
 buttonClick.addEventListener("click", clickTheButton);
 
 function increaseMultiplier() {
     multiScore++;
-    document.querySelector("#multiplaceholder").innerHTML = `Multiplicator x ${multiScore} <br />On the next click the multiplier will cost you x ${penaltyScore*2}`;
+    document.querySelector("#multiplaceholder").innerHTML = `Multiplicator x ${multiScore} <br />Next multiply click cost = ${penaltyScore*2}`;
 }
 
 function penaltyMultiplier() {
@@ -32,3 +36,24 @@ multiClick.addEventListener("click", penaltyMultiplier);
 function doublePenalty() {
     penaltyScore *= 2;
 }
+
+function showHideButton() {
+    if (cookieScore >= autoClickLvl && ticket === 1) {
+        autoClickButt.style.visibility = "visible";
+        document.querySelector("#autoclickph").innerHTML = `Buy Autoclicker <br />Cost = ${autoClickCost}`;
+    } 
+    else {
+        autoClickButt.style.visibility = "hidden";
+    }
+}
+
+function autoClick() {
+    if (cookieScore > autoClickCost) {
+        cookieScore -= autoClickCost;
+        ticket = 0;
+
+        autoClicker = setInterval(clickTheButton, 1000);
+    }
+}
+
+autoClickButt.addEventListener("click", autoClick);
