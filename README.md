ecbfdefbe1548dfd67d3ac3e2b3129e9dcdea1e6
MDE86 <michel.deraemaeker@gmail.com>
2018-10-24 16:22:07 +0200

    upd stage 13 incl

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Javascript-Cookie-clicker
 
-A basic cookie clicker game made with JS. I really struggeled on when to call certain functions and how to make sure the autoclicker button was removed after having it clicked once. I learned to make separata functions for each step in the exercise, instead of nesting functions the whole time like I did during my first try (check script-1try.js in the repo).
+A basic cookie clicker game made with JS. 
+
+## Problems encountered
+
+I really struggeled on when to call certain functions and how to make sure the autoclicker button was removed after having it clicked once. I learned to make separata functions for each step in the exercise, instead of nesting functions the whole time like I did during my first try (check script-1try.js in the repo).
 
 ## Code used
 
@@ -12,7 +16,7 @@ Made during the seventh week of my web development training at BeCode (october 2
 
 ## Status
 
-Finished
+The bonus button is not yet working properly
 
 ## Credits
 
--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ let buttonClick = document.getElementById("clickbutton");
 let cookieCount = document.getElementById("show");
 let multiClick = document.getElementById("multibutton");
 let autoClickButt = document.getElementById("autoclickbutton");
+let bonusButt = document.getElementById("bonusbutton");
 
 let cookieScore = 0;
 let multiScore = 1;
@@ -10,11 +11,13 @@ let autoClicker;
 let autoClickLvl = 20;
 let autoClickCost = 25;
 let ticket = 1; // will help limit the number of times the button is shown to one
+let bonusCost = 200;
 
 function clickTheButton() {
     cookieScore += multiScore;
     cookieCount.innerHTML = cookieScore;
     showHideButton();
+    showHideBonus();
 }
 buttonClick.addEventListener("click", clickTheButton);
 
@@ -41,8 +44,7 @@ function showHideButton() {
     if (cookieScore >= autoClickLvl && ticket === 1) {
         autoClickButt.style.visibility = "visible";
         document.querySelector("#autoclickph").innerHTML = `Buy Autoclicker <br />Cost = ${autoClickCost}`;
-    } 
-    else {
+    } else {
         autoClickButt.style.visibility = "hidden";
     }
 }
@@ -57,3 +59,25 @@ function autoClick() {
 }
 
 autoClickButt.addEventListener("click", autoClick);
+
+let counter = 30;
+let doubleCountdown = setInterval(function () {
+        counter--;
+        // multiScore = multiScore * 2;
+        if (counter === 0) {
+            clearInterval(doubleCountdown);
+        }       
+    }, 1000);
+
+
+function showHideBonus() {
+    if (cookieScore > bonusCost) {
+        bonusButt.disabled = false;
+        document.querySelector("#bonusid").innerHTML = `${counter}`;
+    } else {
+        bonusButt.disabled = true;
+        document.querySelector("#bonusid").innerHTML = "Bonus not available";
+    }
+}
+
+bonusButt.addEventListener("click", doubleCountdown);
